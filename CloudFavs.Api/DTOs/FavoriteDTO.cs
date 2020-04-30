e6211dd56172dbb655aee33ff016ccb5b0c5450f
Sean Schwabenlander <sean@schwabenlander.com>
2020-04-30 17:32:45 +0300

    Added delete confirmation dialog when deleting a favorite.

--- CloudFavs.Api/DTOs/FavoriteDTO.cs
+++ CloudFavs.Api/DTOs/FavoriteDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,22 @@ namespace CloudFavs.Api.DTOs
     public class FavoriteDTO
     {
         public Guid Id { get; set; }
+
         public Guid OwnerId { get; set; }
+
         public Guid FolderId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Uri { get; set; }
+
         public DateTime Created { get; set; }
+
         public DateTime LastModified { get; set; }
+
         public bool IsPinned { get; set; }
     }
 }
--- CloudFavs.Api/DTOs/FolderDTO.cs
+++ CloudFavs.Api/DTOs/FolderDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace CloudFavs.Api.DTOs
     public class FolderDTO
     {
         public Guid Id { get; set; }
+
         public Guid OwnerId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public DateTime Created { get; set; }
+
         public DateTime LastModified { get; set; }
     }
 }
